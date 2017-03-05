dfae0a6c0d55f467a21fd20f44fd9223f2586acd
Yaojie Lu <tanisraistlin@gmail.com>
2017-03-05 11:34:12 +1100

    1

--- optwL/optwL - optimise.r
+++ optwL/optwL - optimise.r
@@ -7,9 +7,10 @@ ca <- 400
 k <- 0.05
 MAP <- 1825
 pkx <- 0.5
-
 c <- 2.64
+
+h3 <- 1
 d <- 5
 
-optwL <- uniroot(optwLif, c(0.156779197039201, 0.160349116248024), tol=.Machine$double.eps^0.25)
+optwL <- uniroot(optwLif, c(0.14489947681764434, 0.156779197039201), tol=.Machine$double.eps^0.25)
 optwL
