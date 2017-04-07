ddf7255b95470f94ee41dcdd5305b1a5ea2e35d8
Yaojie Lu <tanisraistlin@gmail.com>
2017-04-07 19:10:54 +1000

    1

--- OptwL.r
+++ OptwL.r
@@ -4,14 +4,14 @@ source("Functions.r")
 
 # Results
 ca <- 400
-k <- 0.05
-MAP <- 1825
-pkx <- 0.25
 c <- 2.64
 
+k <- 0.05
+MAP <- 1825
 h3 <- 25
-optwL <- vector("list")
+pkx <- 0.5
 
+optwL <- vector("list")
 for(i in 1:10){
   d <- i
   optwL[[i]] <- try(uniroot(optwLif, c(0.1, 0.3), tol=.Machine$double.eps^0.25))
