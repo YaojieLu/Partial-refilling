c2d65abeb4063b73c3d70e0cd94b772d1c3db119
Yaojie Lu <tanisraistlin@gmail.com>
2017-04-10 14:38:23 +1000

    1

--- test.r
+++ test.r
@@ -1,14 +1,20 @@
 
 options(digits=20)
-source("Functions.r")
+source("tf.r")
 
 # Results
 ca <- 400
 c <- 2.64
 
-k <- 0.1
-MAP <- 912.5
+k <- 0.05
+MAP <- 1825
 h3 <- 1
-pkx <- 0.25
-d <- 10
-averBif(0.12527864045, 0.1)
+pkx <- 0.5
+d <- 1
+
+wL <- 0.238815223807604
+x <- seq(0.238815223807604-0.1, 0.238815223807604+0.1, by=0.02)
+y <- numeric()
+for(i in 1:length(x)){
+  y[i] <- averBif(wLi=x[i], wLr=wL)
+}
