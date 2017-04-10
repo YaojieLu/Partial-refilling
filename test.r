50d5967a2ea69a23092bfb4c772de6f88ebb2cf0
Yaojie Lu <tanisraistlin@gmail.com>
2017-04-10 16:30:11 +1000

    1

--- test.r
+++ test.r
@@ -1,22 +1,19 @@
 
 options(digits=20)
-source("tf.r")
+source("Functions.r")
 
 # Results
 ca <- 400
 c <- 2.64
 
-k <- 0.05
-MAP <- 1825
+k <- 0.1
+MAP <- 912.5
 h3 <- 1
-pkx <- 0.5
-d <- 1
+pkx <- 0.25
 
-wL <- 0.24
-x <- seq(0.2, 0.3, by=0.01)
-y <- numeric()
-for(i in 1:length(x)){
-  y[i] <- averBif(wLi=x[i], wLr=wL)
+optwL <- vector("list")
+for(i in 15){
+  d <- i
+  optwL[[i]] <- try(uniroot(optwLif, c(0.1, 0.3), tol=.Machine$double.eps^0.25))
 }
-plot(x, y)
-abline(v=wL)
+optwL
